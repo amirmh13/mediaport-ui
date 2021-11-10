9ea54a40bf9be00d9cb3bcf7a7d69a72b9494ffd
Ali Hashemi <ali_hashemie94@yahoo.com>
2021-11-10 22:01:12 +0330

    shooting schedule done

--- src/app/modules/project/components/shooting-schedule/service/shooting-schedule.service.ts
+++ src/app/modules/project/components/shooting-schedule/service/shooting-schedule.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
-import { IdTitleDto } from '@shared/models';
+import { IdNameDto } from '@shared/models';
 import { ShootingScheduleResult } from '../models/ShootingScheduleResult.model'
 import { ShootingScheduleInitInputDto } from '../models/ShootingScheduleInitInputDto.model'
 import { DaySceneReorder } from '../models/DaySceneReorder.model';
@@ -23,8 +23,8 @@ export class ShootingScheduleService {
     return this._http.get<boolean>(`${environment.apiBaseUrl}/v1/Project/ShootingSchedules/ShootingScheduleExists`, { params: { projectId } });
   }
 
-  getListOfOrderableTypes(): Observable<IdTitleDto[]> {
-    return this._http.get<IdTitleDto[]>(`${environment.apiBaseUrl}/v1/Project/ShootingSchedules/GetListOfOrderableTypes`);
+  getListOfOrderableTypes(): Observable<IdNameDto[]> {
+    return this._http.get<IdNameDto[]>(`${environment.apiBaseUrl}/v1/Project/ShootingSchedules/GetListOfOrderableTypes`);
   }
 
   getListOfShootingScheduleDays(projectId: number): Observable<ShootingScheduleResult[]> {
